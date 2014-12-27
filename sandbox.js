f76e2b1e8ddf3c78bf3f10b328d3060c11f1f24c
Sunil Pai <threepointone@gmail.com>
2014-12-28 04:30:28 +0530

    csp wooo

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,130 @@
-// oia(js <div>something { <span> and something from here </span>}</div>);
+// oia(do
+//   (def x (whilegen (lt x 100) (yieldall (dogen 123 nil)) ))
+//   (prn (into [] (compose (fn [a] a)) x))
+//   )
 
 
-// <div style ={{:width 200}}>
+// oia(lets [go chan put take] (require 'js-csp/lib/csp') (do
+  
+  // (let [c (chan)]
+    
+  //   (go (gen []
+  //     (def i 0)
+  //     (while (lt i 20) 
+  //       (yield (do 
+  //         (js i++) 
+  //         (put c i))))))
+    
+  //   (go (gen []
+  //     (while true 
+  //       (prn (yield (take c)))))))
+  
+  // (let [c (chan)]
+    
+  //   (go (gen []
+  //     (def i 0)
+  //     (while (lt i 20) 
+  //       (yield (do 
+  //         (js i++)
+  //         (prn 'process one putting') 
+  //         (put c i))))))
+    
+  //   (go (gen[] (while true 
+  //     (prn 'process two took' (yield (take c)))))))));
+
+oia(lets [go chan put take timeout alts] (require 'js-csp/lib/csp') (do
+
+  (fn fake [kind] 
+    (fn [c query] 
+      (go (gen [] 
+        (yield (take (timeout (js Math.random()*200))))
+        (yield (put c [kind query]))))))
+
+  (def web1 (fake :web1))
+  (def web2 (fake :web2))
+  (def image1 (fake :image1))
+  (def image2 (fake :image2))
+  (def video1 (fake :video1))
+  (def video2 (fake :video2))
+
+  (fn fastest [query replicas]
+    (let [c (chan)]
+      (.forEach replicas (fn [replica] (replica c query))) c))
+
+  (fn google [query] 
+    (let [c (chan) t (timeout 100) sink (chan)]
+      (go (gen [] 
+        (yield (put c 
+          (yield (take 
+            (fastest query [$ web1 web2])))))))
+      (go (gen [] 
+        (yield (put c 
+          (yield (take 
+            (fastest query [$ image1 image2])))))))
+      (go (gen [] 
+        (yield (put c 
+          (yield (take 
+            (fastest query [$ web1 web2])))))))
+      
+      (go (gen []
+        (def res [$])
+        (dotimes 3 
+          (.push res (get (yield (alts [$ c t])) :value 'timed out')))
+        (yield (put sink res))))
+
+      sink))
+  
+  (go (gen []
+    (prn (yield (take (google 'clojure'))))))));
+
+
+
+// _.go(function* () {
+//   for(var i = 0; i < 10; i++) {      
+//     yield _.put(c, i);
+//    console.log("process one put", i);    
+//   }
+//   c.close()
+// });
+
+// _.go(function* () {
+//   while(true) {
+//     var val = yield _.take(c);
+//     if(val == null) {
+//       break;
+//     } else {
+//       console.log("process two took", val);
+//     }
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+// oia(do
+// 	(def xf 
+//     (compose 
+//       (drop 20)
+//       (map (fn [x] (mul x 3))) 
+//       (filter (fn [x] (eq 0 (mod x 2)))) 
+//       (take 10)))
+  
+//   (prn (seq (range 500) xf)));
+//   // oia(js <div>something { <span> and something from here </span>}</div>);
+
+
+// <div :style {:width 200}>
 // 	text and stuff
 // 	{(.map children (fn [child] <div className 'gambino'>{child}</div> ))}
-// 	<MyComponent key={1785} onClick={this.onClick}/>
+// 	<MyComponent key 1785 onClick this.onClick/>
 // </div>
 
 
@@ -28,15 +148,6 @@
 // 	(def z (.update y (fn [] {:a true :b false})))
 // 	(prn (.cursor x) y z));
 
-// oia(do
-// 	(def xf 
-//     (compose 
-//       (drop 20)
-//       (map (fn [x] (mul x 3))) 
-//       (filter (fn [x] (eq 0 (mod x 2)))) 
-//       (take 10)))
-  
-//   (prn (seq (range 500) xf)));
 
 // oia(let [x 123 y[:a :b :c] z nil](prn x y z));
 
@@ -58,32 +169,41 @@
 // )
 
 
-// var csp = require ('js-csp/lib/csp');
+// var _ = require ('js-csp/lib/csp');
 
-// var c = csp.chan();
-
-// csp.go(function* () {
-//   for(var i = 0; i < 10; i++) {	  	
-//     yield csp.put(c, i);
-//   	console.log("process one put", i);    
-//   }
-//   c.close()
-// });
-
-// csp.go(function* () {
-//   while(true) {
-//     var val = yield csp.take(c);
-//     if(val == null) {
-//       break;
-//     } else {
-//       console.log("process two took", val);
-//     }
-//   }
-// });
+// var c = _.chan();
 
 
 
-// oia(let [x ((gn [] (yield 123)))](prn (.next (doto x (next)))));
+
+// oia(do 
+//   (def x (gen [] (yield 123) (yield 123)))
+//   (def y (gen [] (yieldall (x)) nil))
+  
+//   // (prn (seq (x) (compose (fn [x] x))))
+//   (prn (into [] (compose (fn [a] a)) (y)))
+//   // (prn (.next y))
+//   // (prn(.next y))
+//   // (prn(.next y))
+//   );
+
+
+// (go (gen [] ))
+
+// var y = (function* (){ yield 123; yield 456; yield 789; }());
+
+
+
+// console.log(x.next())
+// console.log(x.next())
+// console.log(x.next())
+// console.log(x.next())
+// console.log(x.next())
+// console.log(x.next())
+
+// (gen [] 
+//   (def r (range 100))
+//   yield())
 
 // oia(prn csp)
 
@@ -91,15 +211,5 @@
 // 	yield 123
 // }
 
-// oia(gn[] (yield 123)(yield 123));
 // var go = require('js-csp/csp.bundled').go
 
-
-// oia(lets [go chan put] (require 'js-csp/lib/csp')
-// 	(let [c (chan)]
-// 		(go (
-// 			gn[]
-// 				(loop [i 0]
-// 					(yield (put c i))
-// 					(if (eq i 100) (yield (put c nil))
-// 						(recur (inc i))))
